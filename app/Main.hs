11a4504c287c05913cd881c34e1e0917efaaf97f
Jose Colomer <joseluis.colomer@tiko.energy>
2023-12-05 22:38:13 +0100

    Revert "Add working API call"
    This reverts commit 60d5a3faa0e712b042d4cb6e20e05280c50fc9bf.

--- app/Main.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Network.HTTP.Simple
-
--- SIMPLE GET
-
-main :: IO ()
-main = do
-    response <- httpLbs "http://transport.opendata.ch/v1/stationboard?station=Siemens&limit=20"
-    print (getResponseBody response)
